02e79e0d8633c14da4b9c6cbd9e43dde6f7baf75
muhammadolammi <muhammadolammi@gmail.com>
2024-06-29 15:09:41 +0100

    migrate

--- src/common/ActionButton.tsx
+++ src/common/ActionButton.tsx
@@ -11,7 +11,7 @@ type Props = {
 const ActionButton = (props: Props) => {
   return (
     <button
-    className={`bg-[#FDF7F2] text-[#161513] font-regular text-[17px] font-poppins py-2 px-4 rounded-lg`}
+    className={`bg-[#FDF7F2] text-[#161513] font-regular text-[17px] font-poppins py-2 px-4 md:h-[50px]  rounded-lg`}
     onClick={()=>props.actionCallback}>
    {props.actionText}
    </button>
