fdbbcdf5d0df7347d23cc5a7546fc4cf9e3f493d
muhammadolammi <muhammadolammi@gmail.com>
2024-07-10 13:56:42 +0100

    add new skills

--- src/views/TutorialsPage.tsx
+++ src/views/TutorialsPage.tsx
@@ -1,81 +1,34 @@
 
 
-import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router'
-import { TutorialsComponent } from '../components/ContentsComponent';
-import {  fetchTutorialsFromPlaylist } from '../db/tutorial';
+
 
 import NavBar from '../common/NavBar';
-import { Playlist,  Tutorial } from '../db/models';
-import {PlaylistBar} from '../common/PlaylistBar';
-import SizedBox from '../common/SizedBox';
-import { fetchPlaylists, getPlaylistWithName } from '../db/playlist';
-import {  DefaultPlaylistName } from '../consts/consts';
+
 
 type Props = {}
 
 const TutorialsPage =  (props: Props) => {
-    const location = useLocation();
-    const playlist:string = location.search.split('=')[1];
 
-    const [tutorials, setTutorials] = useState<Tutorial[]>([]);
-    const [playlists, setPlaylists] = useState<Playlist[]>([]);
-    const [currentPlaylist, setCurrentPlaylist]= useState<Playlist>(
-        {
-            id:"", name:""
-        }
-    )
-
-    const [loading , setLoading] = useState<boolean>(false);
-   
-    //   TODO get tutorial playlist, and set a default playlist
-    //getAndSetcontentswith effect.
-    useEffect(()=>{
-      
-      const fectContents =()=>{
-        getPlaylistWithName(playlist??DefaultPlaylistName).then((playlistData)=>{
-          setCurrentPlaylist(playlistData)
-          fetchPlaylists().then((playlists)=>{
-            setPlaylists(playlists)
-          }).catch((e)=>{
-            console.log(e)
-          }).catch((e)=>{
-            console.log(e)
-          })
-
-        
-        
-        })
-    }
-    setLoading(true)
-    fectContents()
-    setLoading(false)
-  },[playlist, ])
-
-  useEffect(()=>{
-    if(currentPlaylist.id!==""){
-      fetchTutorialsFromPlaylist(currentPlaylist.id).then((tutorials)=>{
-
-        setTutorials(tutorials)
-      })
-     }
-  }, [currentPlaylist])
-
-   
-    // Hanlde to Tutorials
       return(
-      <>
+      <div className='bg-black'>
       <NavBar/> 
-      <div className='Contents-Page'>
-    
-      <div className="contentsTitle">
-            <h1>Tutorials</h1>
+      <div className='body'>
+         <div className=' mt-[50px] w-full bg-red-800 h-[300px]'>
+
+         </div >
+             
+         <div className=' mt-[50px] w-full bg-red-800 h-[300px] text-white'>
+          <p>Recent</p>
+
+
+         </div>
+         <div className=' mt-[50px] w-full bg-red-800 h-[300px] text-white'>
+          <p>Must Read</p>
+
+
+         </div>
+      </div>
         </div>
-        <PlaylistBar playlists={playlists} />
-        <SizedBox height={10}/>
-     <TutorialsComponent  tutorials={tutorials}  loading={loading} />
-        </div>
-        </>
       )
     
  
